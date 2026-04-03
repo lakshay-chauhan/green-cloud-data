0770e8168fea122b80bb4ddd9f208e8f4cca7c07
lakshay-chauhan <lakshay_chauhan@outlook.com>
2026-04-03 07:43:06 +0530

    Update: Integrated Green Code Analyzer with Cloud Simulation

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ WUE = 1.8       # ml/Wh (Water Usage Effectiveness)
 
 API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 class CodePayload(BaseModel):
     code: str
